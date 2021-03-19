974b0378c2d737e94818427b2f2367dea2090547
Mi0103 <mmi01032001@gmail.com>
2021-03-19 14:23:03 +0700

    1st upload to Github.Ahihi

---
+++ src/math/util/MathUtil.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package math.util;
+
+/**
+ *
+ * @author Admin
+ */
+public class MathUtil {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello github");
+        System.out.println("This is 1st message upload that connects"
+                + " to remote server: Github");
+    }
+    
+}
